5757ee9770d238345274e082757c90198546bda6
Ahmed Alani <s3leha@gmail.com>
2020-07-01 18:20:34 -0700

    new query implemented

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -60,9 +60,7 @@ const addUser = function (user) {
   INSERT INTO users (name, email, password) VALUES ($1, $2, $3)RETURNING *;
   `
   return pool.query(queryString, values)
-    .then(res => {
-      return res.rows
-    })
+    .then(res => res.rows)
 }
 exports.addUser = addUser;
 
@@ -86,9 +84,7 @@ const getAllReservations = function (guest_id, limit = 10) {
     LIMIT $2;
   `
   return pool.query(queryString, values)
-    .then(res => {
-      return res.rows;
-    });
+    .then(res => res.rows);
 }
 exports.getAllReservations = getAllReservations;
 
@@ -140,7 +136,9 @@ const getAllProperties = function (options, limit = 10) {
   `;
 
   return pool.query(queryString, values)
-    .then(res => res.rows)
+    .then(res => {
+      return res.rows
+    })
 }
 
 exports.getAllProperties = getAllProperties;
@@ -152,9 +150,13 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const { owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms } = property;
+  let cost_per_night_new = cost_per_night * 100;
+  let values = [owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night_new, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms];
+  const queryString = `
+    INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)RETURNING *;
+  `;
+  return pool.query(queryString, values)
+    .then(res => res.rows[0])
 }
 exports.addProperty = addProperty;
